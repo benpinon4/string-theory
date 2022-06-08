44c94003d9e0f3bd8b0483c7751316b2597af76f
benpinon4 <benjamin.pinon@CodeImmersives.com>
2022-06-08 10:03:27 -0400

    Final Submit

---
+++ main.js
@@ -0,0 +1,98 @@
+console.log("\nxify");
+function xify(str) {
+  let newStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    newStr = newStr + "x";
+  }
+
+  return newStr;
+}
+console.log(xify("Balls"));
+console.log(xify("Happy Nuts"));
+
+console.log("\nyellingChars");
+function yellingChar(str) {
+  let yellingStr = "";
+  for (let y = 0; y < str.length; y++) {
+    yellingStr = yellingStr + str[y] + "!";
+  }
+  return yellingStr;
+}
+console.log(yellingChar("Mellons"));
+
+console.log("\nindexedChars");
+function indexedChars(str) {
+  let indexedStr = "";
+  for (let p = 0; p < str.length; p++) {
+    indexedStr = indexedStr + p + str[p];
+  }
+  return indexedStr;
+}
+console.log(indexedChars("Balls"));
+
+console.log("\nexclaim");
+function exclaim(str) {
+  let sentence = "";
+  for (let s = 0; s < str.length; s++) {
+    if (str[s] === "?" || str[s] === ".") {
+      sentence = sentence + "!";
+    } else {
+      sentence = sentence + str[s];
+    }
+  }
+  return sentence;
+}
+console.log(exclaim("Hello what is your name? My name is Ben."));
+
+console.log("\ntruncate");
+function truncate(str) {
+  let shortStr = "";
+  for (let t = 0; t < 15; t++) {
+    shortStr = shortStr + str[t];
+  }
+  return shortStr + "...";
+}
+console.log(truncate("I love to hug my family"));
+
+console.log("\nciEmailify");
+function ciEmailify(str) {
+  let email = "";
+  for (let e = 0; e < str.length; e++) {
+    if (str[e] === " ") {
+      email = email + ".";
+    } else {
+      email = email + str[e];
+    }
+  }
+  return email + "@codeimmersives.com";
+}
+console.log(ciEmailify("Benjamin Pinon"));
+
+console.log("\nreverse");
+function reversed(str) {
+  let reversed1 = "";
+  for (let r = str.length - 1; r < str.length && r >= 0; r--) { // the loop begins at the last str index and will run when r is greater or eqaul to zero and less than str length. -- counts 1 backwards through each index of the string.
+    reversed1 = reversed1 + str[r];
+  }
+  return reversed1;
+}
+
+console.log(reversed("I love to play with big balls"));
+
+console.log("\nonlyVowels")
+function onlyVowels(str){
+  str = str.toLowerCase()
+  let vowelsOnly = ""
+  for (let o = 0; o < str.length; o++) {
+    
+   if (str[o] === "i" || str[o] === "e" || str[o] === "a" || str[o] === "o" || str[o] === "u"){
+     vowelsOnly = vowelsOnly + str[o]
+  }
+  }
+
+  return vowelsOnly;
+}
+console.log(onlyVowels("JUst dO it"))
+
+
